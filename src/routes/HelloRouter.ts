950213e4b8e31ee0ddc64753f6c6e2d4536f35f9
Candido Acevedo <candidoacevedodev@gmail.com>
2022-08-31 20:03:14 -0400

    HelloController, HelloRouter and rootRouter done

---
+++ src/routes/HelloRouter.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import { HelloController } from '@/controller/HelloController';
+import { LogInfo } from '@/utils/logger';
+
+// Router from express
+let helloRouter = express.Router();
+
+// http://localhost:8000/api/hello/?name=candido
+helloRouter
+	.route('/')
+	// GET:
+	.get(async (req: Request, res: Response) => {
+		// Obtain a Query Param
+		let name: any = req?.query?.name;
+
+		LogInfo(`Query Param: ${name}`);
+
+		// Intance HelloController to excute method
+		const controller: HelloController = new HelloController();
+
+		// Obtain response
+		const response = await controller.getMessage(name);
+
+		// Send to the client the response
+		return res.send(response);
+	});
+
+// Export HelloRouter
+export default helloRouter;
---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Root Router
+ * Redirections to routers
+ */
+import express, { Express, Request, Response } from 'express';
+import helloRouter from './HelloRouter';
+import { LogInfo } from '@/utils/logger';
+
+// Server instance
+let server: Express = express();
+// Router intance
+let rootRouter = express.Router();
+
+// Active for request to http://localhost:8000/api
+
+// GET Initial http://localhost:8000/api
+rootRouter.get('/', (req: Request, res: Response) => {
+	LogInfo('GET: http://localhost:8000/api');
+	res.status(200).send('Hello world, I learning node express full backend');
+});
+
+// Redirections to Routers and Controllers
+server.use('/', rootRouter); //http://localhost:8000/api
+server.use('/hello', helloRouter); //http://localhost:8000/api/hello -> helloRouter
+
+export default server;
